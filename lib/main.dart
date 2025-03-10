4d54aa2058d72e591179f2bbc5c383434c3e783c
deekshithx <dshetty804@gmail.com>
2025-03-10 16:10:23 +0530

    Initail Commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(context) {
+    final MyController c = Get.put(MyController());
+
+    return Scaffold(
+        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
+        body: Center(
+            child: ElevatedButton(
+                child: Text("Go to Other"), onPressed: () => Get.to(Other()))),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add), onPressed: c.increment));
+  }
+}
+
+class Other extends StatelessWidget {
+  final MyController c = Get.find();
+
+  @override
+  Widget build(context) {
+    // Access the updated count variable
+    return Scaffold(body: Center(child: Text("${c.count}")));
+  }
+}
+
+class MyController extends GetxController {
+  var count = 0.obs;
+  increment() => count++;
+}
